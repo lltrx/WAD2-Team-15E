a23895195446d288c80dee7e9d97dc2f7787321b
Alex Chudic <2525381c@student.gla.ac.uk>
2022-03-11 15:29:57 +0100

    reworked population script

--- populate.py
+++ populate.py
@@ -3,11 +3,37 @@ import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'tango_with_django_project.settings')
 import django
-
 django.setup()
-#from rango.models import Category, Page
+from rango.models import Destination
+
+def populate():
+    destinations = {
+        'Glasgow Necropolis': {'description': """The Glasgow Necropolis is a Victorian cementary in Glasgow, Scotland.
+                                          It is on a low, but very prominent hill to the east of Glasgow Central."""},
+        'Edinburght City Centre': {'description': """Edinburgh's busy city centre is ranked amongst the most attractive in the world.
+
+                                                Exploring the medieval Old Town, you will discover the secrets of Edinburgh's past 
+                                                and can visit some of the city's top attractions, including Edinburgh Castle.
+                                                Venture on to bustling Princes Street and New Town to shop until you drop,
+                                                 taking in the architectural grandeur of the Capital and its landmarks."""}
+    }
+
+    for dest, dest_info in destinations.items():
+        add_destination(dest, dest_info['description'])
+        print(dest)
 
 
+
+def add_destination(name, description):
+    d = Destination.objects.get_or_create(name=name)[0]
+    d.description = description
+    d.save()
+
+if __name__ == '__main__':
+    print('Starting Rango population script...')
+    populate()
+
+'''
 def populate():
     python_pages = [
         # We can add some example of places here 
@@ -72,8 +98,4 @@ def add_cat(name,views,likes):
     c.likes = likes
     c.save()
     return c
-
-
-if __name__ == '__main__':
-    print('Starting Rango population script...')
-    populate()
+'''
